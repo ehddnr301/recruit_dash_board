d7b503a88a686f630c92ce4c8247f4d012343844
ehddnr301 <dy950328@gmail.com>
2021-07-25 19:30:40 +0900

    Show Map & info table

--- main.py
+++ main.py
@@ -5,18 +5,38 @@ import plotly.express as px
 import pandas as pd
 from sim import simirality_df, Similarity
 
-sim = Similarity(simirality_df, 'ehddnr', 'python@javascript@AWS@Django@브랜드마케팅', 'cosine')
+lat_lon_df = pd.read_csv('./lat_lon_df.csv', sep=';')
+sim = Similarity(simirality_df, 'ehddnr', '콘텐츠디자인@UI/UX디자인@웹디자인@Sketch@figma', 'cosine')
 
 idxs = sim.get_item()
 
 # if idxs:
 df = pd.DataFrame(idxs, columns=['recruit_id'])
 
+my_recruit_df = pd.merge(left=df, right=lat_lon_df, how='left', on='recruit_id')
+my_recruit_df['info_address'].fillna('재택', inplace=True)
+my_recruit_df.fillna(0, inplace=True)
+my_recruit_df['lat'] = my_recruit_df['lat'].apply(lambda x : round(x,4))
+my_recruit_df['lon'] = my_recruit_df['lon'].apply(lambda x : round(x,4))
+
+map_df = my_recruit_df[my_recruit_df['lat'] != 0]
 
 stylesheets = [
     "https://cdn.jsdelivr.net/npm/reset-css@5.0.1/reset.min.css",
     "https://fonts.googleapis.com/css2?family=Open+Sans&display=swap",
 ]
+
+fig = px.scatter_mapbox(map_df,
+                        lat='lat',
+                        lon='lon',
+                        color='recruit_id',
+                        hover_data=['info_address'],
+                        color_continuous_scale=px.colors.cyclical.IceFire,
+                        zoom=10,
+                      )
+
+fig.update_layout(mapbox_style='carto-positron')
+fig.update_layout(margin={'r':0,'t':0,'l':0,'b':0})
 
 
 app = dash.Dash(__name__, external_stylesheets=stylesheets)
@@ -30,18 +50,26 @@ app.layout = html.Div(
     },
     children=[
         html.Header(
-            style={"textAlign": "center", "paddingTop": "50px"},
+            style={"textAlign": "center", "paddingTop": "50px", 'marginBottom': '50px'},
             children=[html.H1("Locket Punch", style={"fontSize": 50})],
         ),
         html.Div(
+            style={'display':'flex', 'alginItems':'center', 'justifyContent': 'center'},
             children=[
+                html.Div(
+                    style={"marginRight": "30px"},
+                    children=[
+                        dcc.Graph(figure=fig)
+                    ]
+                ),
                 html.Table(
+                    style={'textAlign':'center'},
                     children=[
                     html.Thead(
                         children=[
                             html.Tr(
                                 children=[
-                                    html.Th(col) for col in df.columns
+                                    html.Th(col) for col in my_recruit_df.columns
                                 ]
                             )
                         ]
@@ -50,10 +78,13 @@ app.layout = html.Div(
                         children=[
                             html.Tr(
                                 children=[
-                                    html.Td(val)  for val in values
+                                    html.Td(
+                                        style={'padding':'10px'},
+                                        children=[val]
+                                    )  for val in values
                                 ] 
                             ) 
-                            for values in df.values
+                            for values in my_recruit_df.values
                         ]
                     )
                   ]
